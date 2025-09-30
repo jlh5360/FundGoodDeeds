1ea688ada228bfb9272e84f4ef601a49d88bfe34
jlh5360 <jlh5360@rit.edu>
2025-09-29 20:04:38 -0400

    Completed Activity 6 task 1 (defineed an appropriate Java interface IBarometer) and task 2 (refactored the simulated Barometer class to implement the IBarometer interface)

---
+++ src/IBarometer.java
@@ -0,0 +1,7 @@
+/**
+ * A class can implement the {@code IBarometer} interface when it
+ * promises to provide an implementation for all of its methods. 
+ */
+public interface IBarometer {
+    double pressure() ;
+}
