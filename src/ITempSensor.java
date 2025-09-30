3a9a4ec261ea507bce18ccb97d4ca851d24a15d4
cdb9772-cloud <cdb9772@g.rit.edu>
2025-09-29 20:53:45 -0400

    Completed: Activity 6 tasks 3 & 4 (create ITempSensor interface and kelvin temp sensor adapter)

---
+++ src/ITempSensor.java
@@ -0,0 +1,25 @@
+//Author: Connor Bashaw
+//date: 9/29/25
+//ItempSensor.java
+
+/**
+ * general temp sensor, that any concree temperature sensor, can implement or be adapted to
+ * values are returned in doubles, in the requested temp scale.
+ */
+
+public interface ITempSensor {
+
+    // get current temp in degrees celsius
+    double getCelsius();
+    
+    //get temp in farenheit
+    double getFarenheit();
+    
+    //get temp in kelvin
+    double getKelvin();
+
+
+    default String getName(){
+        return this.getClass().getSimpleName();
+    }
+}
---
+++ src/KelvinTempSensorAdapter.java
@@ -0,0 +1,43 @@
+//Author: Connor Bashaw
+//date: 9/29/25
+//KelvinTempSensorAdapter.java
+
+/** Adapter that wraps kelvinTempSensor and exposes to general temp sensor.
+ * Assumptions (based on the WeatherStation code):
+ *  - KelvinTempSensor.reading() returns an integer: Kelvin * 100 (i.e., centi-Kelvin).
+ *  - Celsius = Kelvin - 273.15
+ *  - Fahrenheit = (Celsius * 1.8) + 32
+ */
+
+public class KelvinTempSensorAdapter implements ITempSensor{
+    private final KelvinTempSensor adaptee;
+
+    //conversion constants
+    private static final int KTOC = -27315; 
+    private static final double HUNDRED = 100.0;
+    
+    public KelvinTempSensorAdapter(KelvinTempSensor adaptee){
+        if (adaptee == null) {
+            throw new IllegalArgumentException("Adaptee KelvinTempSensor cannot be null");
+        }
+        this.adaptee = adaptee;
+    }
+
+    @Override
+    public double getKelvin(){
+        // reading returns kelvin as int
+        return adaptee.reading() / HUNDRED;
+    }
+
+    @Override
+    public double getCelsius() {
+        // ( kelvin + KTOC) / 100
+        return (adaptee.reading() + KTOC) / HUNDRED;
+    }
+
+    @Override
+    public double getFarenheit() {
+        // (celsius *1.8)  + 32
+        return (getCelsius() *1.8) + 32;
+    }
+}
