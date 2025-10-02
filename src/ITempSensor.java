94e518ca4baf02153862ac5ffd42d38dd007e1a1
DE4Fxx <patrickmicheallebeau@gmail.com>
2025-10-02 02:36:28 -0400

    Resolving merge conflict with ITempSensor interface

--- src/ITempSensor.java
+++ src/ITempSensor.java
@@ -1,9 +1,12 @@
-/**
- *  ITempSensor interface
- *  
- */
+public interface ITempSensor {
+        // get current temp in degrees celsius
+    public double getCelsius();
+    
+    //get temp in farenheit
+    public double getFarenheit();
+    
+    //get temp in kelvin
+    public double getKelvin();
 
-public interface ITempSensor 
-{
     public int reading();
 }
