09affa133a048cc3e08b9d9e4abefa95b7bb55ce
jlh5360 <jlh5360@rit.edu>
2025-10-19 22:44:14 -0400

    Created some MVC skeleton classes and code

---
+++ src/Controller/LedgerController.java
@@ -0,0 +1,48 @@
+package Controller;
+
+public class LedgerController {
+	private final Needsrepository needsRepository;
+	private final Ledgersrepository ledgerRepository;
+
+	public LedgerController(NeedsRepository needsRepository, ledgerRepository ledgerRepository) {
+		this.needsRepository = needsRepository;
+		this.ledgerRepository = ledgerRepository;
+	}
+
+	public void recordsFullfillment(String needName, int count) {
+		LedgerEntity fulfillment = new LedgerEntity(LocalDate.now(), LedgerEntity.EntryType.NEED, needName, count);
+
+		ledgerRepository.save(fulfillment);
+
+		needsRepository.getNeedByName(needName);
+	}
+		
+	public void displaySummary(ConsoleView view) {
+		String summary = ledgerRepository.getSummary();
+
+		view.displaySummary(summary);
+    }
+        
+    public void processDailyGoal(ConsoleView view) {
+        LocalDate today = LocalDate.now();
+        int need_count = 5; // Placeholder for loop condition
+
+        while (need_count > 0) {
+            double goal = ledgerRepository.findGoal(today);
+            double donations = ledgerRepository.calculateDonations(today);
+            double remainingGoal = goal - donations;
+
+            view.displayGoal(remainingGoal);
+
+            need_count = (need_count - 1);
+        }
+    }
+
+	public void registerDonations(double donation1, double donation2, ConsoleView view) {
+		ledgerRepository.addDonations(donation1, donation2);
+
+		//Logic to recalculate the goal status
+
+		view.donationCompleted();
+	}
+}
---
+++ src/Model/LedgerEntity.java
@@ -0,0 +1,41 @@
+package Model;
+
+public class LedgerEntity {
+	public enum EntryType {FUND, GOAL, NEED}
+
+	private final LocalDate date;
+private final EntryType type;
+private final String needName;
+private final double amount;
+private final int count;
+
+//Constructor for FUND/DOAL entries
+public LedgerEntity(LocalDate date, EntryType type, double amount) {
+	this.date = date;
+	this.type = type;
+	this.amount = amount;
+	this.needName = null;
+	this.count = 0;
+}
+
+//Contructor for NEED fulfillment entries
+public LedgerEntity(LocalDate date, EntryType type, String needName, int count) {
+	this.date = date;
+	this.type = type;
+	this.needName = needName;
+	this.count = count;
+	this.amount = 0.0;
+}
+
+	public String getNeedName() {
+		return needName;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+}
