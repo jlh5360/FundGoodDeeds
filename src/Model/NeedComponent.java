e7ffbd81e85a4faeb360589b295973f3bc0ecf3d
jlh5360 <89438794+jlh5360@users.noreply.github.com>
2025-10-19 23:03:04 -0400

    Created some MVC skeleton classes and code (#10)

---
+++ src/Model/NeedComponent.java
@@ -0,0 +1,18 @@
+package Model;
+
+public abstract class NeedComponent {
+	protected String name;
+
+	public NeedComponent(String name) {
+		this.name = name;
+	}
+
+	public abstract double calculateTotalCost();
+	public abstract double calculateFixedCost();
+	public abstract double calculateVariableCost();
+	public abstract double calculateFeeCost();
+
+	public String getName() {
+		return name:
+	}
+}
