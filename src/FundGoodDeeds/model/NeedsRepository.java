481ab0400a016b0ee510fa7ff1eedeb41098fd17
OGxLuxuries <ogluxuries@gmail.com>
2025-10-30 22:41:58 -0400

    pulled Patricks changes

--- src/FundGoodDeeds/model/NeedsRepository.java
+++ src/FundGoodDeeds/model/NeedsRepository.java
@@ -1,8 +1,10 @@
-package Model;
+package FundGoodDeeds.model;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
+import FundGoodDeeds.*;
 
 public class NeedsRepository extends Observable {
 
@@ -49,7 +51,7 @@ public class NeedsRepository extends Observable {
 		List<NeedComponent> needsList = new ArrayList<>();
 		for(String[] need : rawNeeds)
 		{
-			NeedComponent needComponent = new Need(need[1], Double.parseDouble(need[2]),Double.parseDouble(need[3]),Double.parseDouble(need[4]));
+			NeedComponent needComponent = new Need(need[1], Double.parseDouble(need[2]),Double.parseDouble(need[3]),Double.parseDouble(need[4]), Double.parseDouble(need[5]));
 			needsList.add(needComponent);
 		}
 		return needsList;
@@ -113,8 +115,23 @@ public class NeedsRepository extends Observable {
 					count = Double.parseDouble(bundle[index]);
 				}
 				// Ensure we don't accidentally add the placeholder values
-				if(count > -1 && !(needName.equals("undefined")))
-					bundleObject.addComponent(needName, (int) count);
+				if(count > -1 && !(needName.equals("undefined"))) {
+					
+					// Look up the need from the catalog - prevents adding needs that don't exist
+                	NeedComponent need = getNeedByName(needName);
+                
+					if(need != null) {
+						// Add the need 'count' times to the bundle
+						for(int i = 0; i < (int)count; i++)
+						{
+							bundleObject.add(need);
+						}
+					}
+
+				}
+            		
+                
+            	
 			}
 			bundles.add(bundleObject);
 		}
