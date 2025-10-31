8cdaa7a351b468837eff80f10b9a74330c6912ab
cdb9772-cloud <cdb9772@g.rit.edu>
2025-10-30 22:08:47 -0400

    initial commit of connors working skeleton. Comments. Please contact … (#22)
    * initial commit of connors working skeleton. Comments. Please contact for questions
    
    * 2nd commit for work. realize mistake 1 . working on fix for 2
    
    * forgot to add observer in app , also made naming more clear
    
    * update view with observer  pattern in constructor, and update
    
    * remove custom observer and observable bad idea, use deprecated java util

--- src/FundGoodDeeds/app/FundGoodDeedsApp.java
+++ src/FundGoodDeeds/app/FundGoodDeedsApp.java
@@ -1,17 +1,51 @@
+/**
+ * The main entry point for the FundGoodDeeds App.
+ * Acts as the Composition Root, setting up the MVC and injecting dependencies.
+ * Connor Bashaw - FundGoodDeedsApp.java
+ */
+
+
 package FundGoodDeeds.app;
 
-import controller.*; 
-import repository.*; 
-import view.*;
+import FundGoodDeeds.controller.LedgerController;
+import FundGoodDeeds.controller.NeedsController;
+import FundGoodDeeds.model.CSVManager;
+import FundGoodDeeds.model.LedgerRepository;
+import FundGoodDeeds.model.NeedsRepository;
+import FundGoodDeeds.view.ConsoleView;
+
+
 
 public class FundGoodDeedsApp {
     public static void main(String[] args){
-        NeedsRepository nRepo=new NeedsRepository();
-        LedgerRepository lRepo=new LedgerRepository();
-        NeedsController nCtrl=new NeedsController(nRepo);
-        LedgerController lCtrl=new LedgerController(lRepo);
-        ConsoleView view=new ConsoleView();
-        System.out.println("FundGoodDeeds skeleton running.");
+        //1. Model 
+        //Instantiate csv manager
+        CSVManager csvManager = new CSVManager();
+
+        //Instantiate Repositories and inject CSVManager
+        NeedsRepository needsRepo = new NeedsRepository(csvManager);
+        LedgerRepository ledgerRepo = new LedgerRepository(csvManager);
+        
+        // 2. Controller  
+        //Instantiate Controllers, inject Repositories (Model)
+        NeedsController needs=new NeedsController(needsRepo);
+        LedgerController ledger=new LedgerController(ledgerRepo ,needsRepo);
+
+        
+        //3. VIEW IMPORTANT TO CONNOR WORK
+        //Instantiate View and inject Controllers into View (View knows Controller controller knows repo)
+        ConsoleView view=new ConsoleView(needs, ledger);
+
+        // Register view as observer 
+        needs.addObserver(view);
+        ledger.addObserver(view);
+        
+
+        //4.STARTUP
+        //Initialize the application via the View/Controller
+        //(Sequence Diagram #1: ConsoleView -> startup() initiates the load)
+        view.startup();
+
     }
 }
 
--- src/FundGoodDeeds/common/Observer.java
+++
@@ -1,6 +0,0 @@
-package FundGoodDeeds.view;
-
-@FunctionalInterface
-public interface Observer<T> { 
-    void update(T arg); 
-}
